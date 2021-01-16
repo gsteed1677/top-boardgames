3a17a31e16ead6983c9a63f74412fedc041abca8
alexaergun <alexaergun@gmail.com>
2021-01-16 10:31:25 -0700

    MVC working, pushing up to get the handlbars pages rendered and work on the API

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,27 +1,54 @@
 var path = require("path");
+const { ConnectionError } = require("sequelize/types");
+// var router = express.Router()
+
 
 // Requiring our custom middleware for checking if a user is logged in
 var isAuthenticated = require("../config/middleware/isAuthenticated");
 
 module.exports = function (app) {
-  app.get("/signup", function (req, res) {
-    // If the user already has an account send them to the members page
-    if (req.user) {
-      res.redirect("/users");
-    }
-    res.sendFile(path.join(__dirname, "../public/signup.html"));
-  });
+  app.get('/',  (req, res) => {
+    res.render('index')
+  })
 
-  app.get("/login", function (req, res) {
-    // If the user already has an account send them to the members page
-    if (req.user) {
-      res.redirect("/users");
-    }
-    res.sendFile(path.join(__dirname, "../public/login.html"));
-  });
+  app.get('/signup',  (req, res) => {
+    res.render('index')
+  })
 
-  // If a user who is not logged in tries to access this route they will be redirected to the signup page
-  app.get("/users", isAuthenticated, function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/user.html"));
-  });
+  app.get('/login',  (req, res) => {
+    res.render('index')
+  })
+
+  app.get('/contact',  (req, res) => {
+    res.render('index')
+  })
+
+  app.get('/form',  (req, res) => {
+    res.render('index')
+  })
+
+  app.get('/recommended',  (req, res) => {
+    res.render('index')
+  })
+
+  // app.get("/signup", function (req, res) {
+  //   // If the user already has an account send them to the members page
+  //   if (req.user) {
+  //     res.redirect("/users");
+  //   }
+  //   res.sendFile(path.join(__dirname, "../public/signup.html"));
+  // });
+
+  // app.get("/login", function (req, res) {
+  //   // If the user already has an account send them to the members page
+  //   if (req.user) {
+  //     res.redirect("/users");
+  //   }
+  //   res.sendFile(path.join(__dirname, "../public/login.html"));
+  // });
+
+  // // If a user who is not logged in tries to access this route they will be redirected to the signup page
+  // app.get("/users", isAuthenticated, function (req, res) {
+  //   res.sendFile(path.join(__dirname, "../public/user.html"));
+  // });
 };
