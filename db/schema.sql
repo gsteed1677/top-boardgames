5c9f41d4f6a7b245526abbc7bdacbe9cd03eaa15
alexaergun <alexaergun@gmail.com>
2021-01-16 14:00:39 -0700

    edit workbench modified schema and config  created topgame.js file

--- db/schema.sql
+++ db/schema.sql
@@ -1,14 +1,17 @@
-DROP DATABASE IF EXISTS top_boardgamesDB;
-CREATE database top_boardgamesDB;
+DROP DATABASE IF EXISTS topboardgames_DB;
+CREATE database topboardgames_DB;
 
-USE top_boardgamesDB;
+USE topboardgames_DB;
 
 CREATE TABLE top100 (
-  rating INT NOT NULL,
-  genre VARCHAR NULL,
+
   game VARCHAR NULL,
-  players INT NULL
+  players INT NULL ,
+  rating INT NOT NULL,
+  genre VARCHAR NULL
+ 
+  
 
 );
 
-SELECT * FROM top100;
+SELECT * FROM top100games;
