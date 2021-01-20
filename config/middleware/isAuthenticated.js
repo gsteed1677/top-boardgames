cbd0171952e85403fbf663854c48fcfe1596a9ef
alexaergun <alexaergun@gmail.com>
2021-01-20 06:03:28 -0700

    making it work

--- config/middleware/isAuthenticated.js
+++ config/middleware/isAuthenticated.js
@@ -1,4 +1,4 @@
-module.exports = function(req, res, next) {
+export default function(req, res, next) {
   // If the user is logged in, continue with the request to the restricted route
   if (req.user) {
     return next();
